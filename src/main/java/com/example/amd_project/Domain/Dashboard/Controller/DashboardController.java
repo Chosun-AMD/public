efbd5367660fbec54b42a1a4ec987278eaada260
ghkdtlwns987 <ghkdtlwns987@naver.com>
2023-05-27 23:36:13 +0900

    dashboardController

--- src/main/java/com/example/amd_project/Domain/Dashboard/Controller/DashboardController.java
+++ src/main/java/com/example/amd_project/Domain/Dashboard/Controller/DashboardController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/")
 public class DashboardController {
     private final DashboardServiceImpl dashboardService;
 
